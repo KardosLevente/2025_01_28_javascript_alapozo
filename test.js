ffad22c96eb3b42500a62aeb9649b69735688942
KardosLevente <kardos.levente@diak.szbi-pg.hu>
2025-01-28 09:28:34 +0100

    image buttons

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+function kep_beillesztes()
+{
+    document.getElementById("kep_helye").innerHTML = "<img src='kanye.jpg'>";
+}
+function kep_torles()
+{
+    document.getElementById("kep_helye").innerHTML = "";
+}
